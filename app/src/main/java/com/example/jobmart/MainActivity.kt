92df717456f70d60528ff6f9857ee918d4715b2c
Krishna Khadka <krishna.khadka@mavs.uta.edu>
2020-11-20 09:11:45 -0600

    fragment added

--- app/src/main/java/com/example/jobmart/MainActivity.kt
+++ app/src/main/java/com/example/jobmart/MainActivity.kt
@@ -1,26 +1,30 @@
 package com.example.jobmart
 
+
+
 import android.os.Bundle
-import android.os.Message
-import android.widget.TextView
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
-import com.example.jobmart.fragments.HomeFragment
-import com.example.jobmart.fragments.NotificationsFragment
-import com.example.jobmart.fragments.ProfileFragment
-import com.example.jobmart.fragments.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
 
     }
+
+
 }
