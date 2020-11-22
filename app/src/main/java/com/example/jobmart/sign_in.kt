045323d2501b29a46fb4fa321e9190c20219401e
Krishna Khadka <krishna.khadka@mavs.uta.edu>
2020-11-21 18:22:15 -0600

    firebase added

--- app/src/main/java/com/example/jobmart/sign_in.kt
+++ app/src/main/java/com/example/jobmart/sign_in.kt
@@ -2,10 +2,26 @@ package com.example.jobmart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class sign_in : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        auth = FirebaseAuth.getInstance()
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser: FirebaseUser? = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    fun updateUI(currentUser: FirebaseUser?){
+
     }
 }
