e02cbef40f9b00ef0e7e380104aa6df881e2b808
Krishna Khadka <krishna.khadka@mavs.uta.edu>
2020-11-21 19:43:10 -0600

    firebase added

--- app/src/main/java/com/example/jobmart/SignIn.kt
+++ app/src/main/java/com/example/jobmart/SignIn.kt
@@ -1,29 +1,30 @@
 package com.example.jobmart
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
-class sign_in : AppCompatActivity() {
+
+class SignIn : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
         auth = FirebaseAuth.getInstance()
-        btn_signup.setOnClickListener {
 
-        }
     }
 
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser: FirebaseUser? = auth.currentUser
-        updateUI(currentUser)
+        UpdateUI(currentUser)
     }
 
-    fun updateUI(currentUser: FirebaseUser?){
+    fun UpdateUI(currentUser: FirebaseUser?){
 
     }
 }
--- app/src/main/java/com/example/jobmart/SignUp.kt
+++ app/src/main/java/com/example/jobmart/SignUp.kt
@@ -3,7 +3,7 @@ package com.example.jobmart
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class sign_up : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
