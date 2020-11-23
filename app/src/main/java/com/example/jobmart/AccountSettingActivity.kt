4af82671b647d10563a550d49811a777a10287b8
rozan123 <66568997+rozan123@users.noreply.github.com>
2020-11-23 16:04:06 -0600

    add

---
+++ app/src/main/java/com/example/jobmart/AccountSettingActivity.kt
@@ -0,0 +1,16 @@
+package com.example.jobmart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_account_setting.*
+
+class AccountSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_setting)
+
+
+
+    }
+}
--- app/src/main/java/com/example/jobmart/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/jobmart/fragments/ProfileFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.jobmart.R
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,7 +36,9 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        return view
     }
 
     companion object {
