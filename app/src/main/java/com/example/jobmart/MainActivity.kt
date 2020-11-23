41a39a6ae0298aa9e0a6dac8149a82758acbab4d
Krishna Khadka <krishna.khadka@mavs.uta.edu>
2020-11-23 15:58:35 -0600

    login

--- app/src/main/java/com/example/jobmart/MainActivity.kt
+++ app/src/main/java/com/example/jobmart/MainActivity.kt
@@ -2,34 +2,26 @@ package com.example.jobmart
 
 
 
+
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
-import android.view.WindowManager
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_sign_in.*
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var auth: FirebaseAuth
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpNavigation();
 
 
 
@@ -41,5 +33,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun setUpNavigation() {
+        bottomNavigationView = findViewById(R.id.bottom_nav_view)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+        NavigationUI.setupWithNavController(
+            bottomNavigationView,
+            navHostFragment!!.navController
+        )
+    }
 
 }
--- app/src/main/java/com/example/jobmart/SignUp.kt
+++ app/src/main/java/com/example/jobmart/SignUp.kt
@@ -1,5 +1,6 @@
 package com.example.jobmart
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,9 +10,13 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import kotlinx.android.synthetic.main.activity_sign_up.password
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 
 class SignUp : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var userreference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,10 @@ class SignUp : AppCompatActivity() {
         }
     }
     private fun signupuser(){
+        val Fnames = first_name.text.toString()
+        val Lnames = last_name.text.toString()
+        val Mnames = middle_name.text.toString()
+        val emails = email.text.toString()
         if(email.text.toString().isEmpty()){
             email.error = "Please enter email"
             email.requestFocus()
@@ -39,11 +48,19 @@ class SignUp : AppCompatActivity() {
             password.requestFocus()
             return
         }
+
         auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    startActivity(Intent(this,MainActivity::class.java))
-                    finish()
+                    val currentUser = auth.currentUser
+                    currentUser!!.sendEmailVerification()
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                store(Fnames, Mnames, Lnames, emails)
+                            }
+                        }
+                //    startActivity(Intent(this,MainActivity::class.java))
+                  //  finish()
                 } else {
                     Toast.makeText(baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
@@ -52,4 +69,26 @@ class SignUp : AppCompatActivity() {
 
             }
     }
+    private fun store(Fnames: String, Mnames: String, Lnames: String, emails: String) {
+
+        var curruserId = auth.currentUser!!.uid
+        userreference = FirebaseDatabase.getInstance().reference.child("usersID").child(curruserId)
+
+        val currUserHashMap = HashMap<String, Any>()
+
+        currUserHashMap["usersID"] = curruserId
+        currUserHashMap["FName"] = Fnames.toLowerCase()
+        currUserHashMap["MName"] = Mnames.toLowerCase()
+        currUserHashMap["LName"] = Lnames.toLowerCase()
+        currUserHashMap["Email"] = emails
+        userreference.updateChildren(currUserHashMap)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Sign up Success.", Toast.LENGTH_LONG).show();
+                    startActivity(Intent(this@SignUp, MainActivity::class.java))
+                    finish()}
+            }
+
+    }
+
 }
